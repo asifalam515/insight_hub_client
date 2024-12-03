813136cfbb67e071d65cd1f10d58cf788e956525
asifalam515 <asibulalam515@gmail.com>
2024-12-03 20:44:35 +0600

    firebase started

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBp6QOgsFOBDKPlvMh8cQ6mJm1Oqrt5O1Q",
+  authDomain: "insight-hub-2a560.firebaseapp.com",
+  projectId: "insight-hub-2a560",
+  storageBucket: "insight-hub-2a560.firebasestorage.app",
+  messagingSenderId: "374405185043",
+  appId: "1:374405185043:web:cbb9fc3cba2c75923cc9e6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
