4d226f03f8114f596ff2900cf1e50f323aded527
asifalam515 <asibulalam515@gmail.com>
2024-12-03 15:58:55 +0600

    navbar added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Home from "../pages/HomePage/Home/Home";
+import Navbar from "../pages/HomePage/Navbar/Navbar";
 
 const Root = () => {
   return (
     <div>
+      <Navbar></Navbar>
+      <Home></Home>
       <Outlet></Outlet>
     </div>
   );
---
+++ src/pages/HomePage/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      {/* this is main home page component. Everything in homepage will takes place here */}
+    </div>
+  );
+};
+
+export default Home;
